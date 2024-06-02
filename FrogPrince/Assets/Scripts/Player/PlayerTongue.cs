173ea32bcf517639de0175bc5e788542ae0fcfd0
shinwoopark <shinwoo060310@gmail.com>
2024-06-03 02:17:53 +0900

    first

---
+++ FrogPrince/Assets/Scripts/Player/PlayerTongue.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerTongueAction : MonoBehaviour
+{
+    public PlayerMove PlayerMove;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision != null)
+        {
+            if (collision.gameObject.layer == 7 ||
+                collision.gameObject.layer == 8 ||
+                collision.gameObject.layer == 9)
+            {
+                PlayerMove.HitTongue("platform");
+            }
+            else if (collision.gameObject.layer == 6)
+            {
+                PlayerMove.HitTongue(collision.gameObject.tag);
+            }
+        }
+    }
+}
